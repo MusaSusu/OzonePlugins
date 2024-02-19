930c80326b9e052d03afda3367a3284318c0f298
musasu <nekomaobusiness@gmail.com>
2024-02-19 13:01:57 -0500

    clean up config UI.

--- Wintertodt/src/main/java/Ozone/Wintertodt/WintertodtConfig.java
+++ Wintertodt/src/main/java/Ozone/Wintertodt/WintertodtConfig.java
@@ -7,28 +7,18 @@ import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("Ozone-WintertodtConfig")
 public interface WintertodtConfig extends Config {
-    @ConfigSection(
-            position = 0,
-            keyName = "Ozone-Wintertodt",
-            name = "Ozone Wintertodt Assistant",
-            description = ""
-    )
-    String WintertodtConfig = "Wintertodt Config";
     @ConfigItem(
             position = 0,
             keyName = "Revive Pyromancer",
             name = "Revive Pyromancer",
-            description = "Enable to auto revive pyromancer if close.",
-            section = WintertodtConfig
+            description = "Enable to auto revive pyromancer if close."
     )
     default boolean isRevive() {return false;}
-
     @ConfigItem(
             position = 1,
             keyName = "Health threshold",
             name = "Health threshold",
-            description = "Eat when you are below this health value",
-            section = WintertodtConfig
+            description = "Eat when you are below this health value"
     )
     default int getHPThresh() {return 20;}
 }
